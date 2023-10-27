0ec4b54f304c776d3dba68301e96785110d7f039
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-10-27 15:00:56 -0600

    Commit de las 3 de la tarde :3

--- Lab3P3_DanielElvir.cpp
+++ Lab3P3_DanielElvir.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 void menu() {
     cout << "1. Aproximacion de PI" << endl;
@@ -8,27 +10,58 @@ void menu() {
     cout << "Escoja una de las opciones validas" << endl;
 }
 
-void ejercicio1() {
-    int puntos;
+void ejercicio1() {    
+    double puntos;
+    double puntosDentro=0;
+    double valorPI;
     bool verify = true;
-    cout << "Ingrese el total de puntos que se deben generar (tiene que ser mayor o igual a 1000000: ";
+    cout << "Ingrese el total de puntos que se deben generar (tiene que ser mayor o igual a 1000000): ";
     cin >> puntos;
     while (verify){
         if (puntos>=1000000){
+            for (int i = 0; i < puntos; i++) {
+                double x = (rand() / (double)RAND_MAX) * 2.0 - 1.0;
+                double y = (rand() / (double)RAND_MAX) * 2.0 - 1.0;
+                if (x*x+y*y<=1){
+                    puntosDentro++;
+                }
+            }
 
+            valorPI = 4 * (puntosDentro / puntos);
+
+            cout << "Estimacion de PI usando Montecarlo con " << puntos << " puntos generados es: " << valorPI << endl;
+            cout << "" << endl;
+            break;
         } else {
-            cout << "Ingrese un numero valido"
+            cout << "Ingrese un numero valido" << endl;
+            cout << "Ingrese el total de puntos que se deben generar (tiene que ser mayor o igual a 1000000): ";
+            cin >> puntos;
         }
     }
 
+}   
+
+int calcularPell(int n) {
+    if (n==1 || n==0) {
+        return n;
+    } else if(n>=2) {
+        return 2 * calcularPell(n - 1) + calcularPell(n - 2);
+    }
 }
 
 void ejercicio2() {
-
+    int n;
+    int res;
+    cout << "Ingrese n: ";
+    cin >> n;
+    for (int i = 0; i <= n; i++) {                
+        res = calcularPell(i);
+        cout << "P(" << i << ") = " << res << endl;
+    }
 }
 
 void ejercicio3() {
-
+    
 }
 
 int main() {
@@ -39,13 +72,13 @@ int main() {
         cin >> opcion;
         switch (opcion) {            
             case 1:                 
-                cout << "En construccion el primero" << endl;
+                ejercicio1();
                 break;
             case 2:
-                cout << "En construccion el segundo" << endl;
+                ejercicio2();
                 break;
             case 3:
-                cout << "En construccion el tercero" << endl;
+                ejercicio3();
                 break;
             case 4:
                 cout << "Saliendo del Programa..." << endl;

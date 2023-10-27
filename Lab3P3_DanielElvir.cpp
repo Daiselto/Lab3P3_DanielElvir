a2cffad9643e0a5f941a27ee7320ff075df45f1a
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-10-27 15:44:21 -0600

    Ultimo Commit

--- Lab3P3_DanielElvir.cpp
+++ Lab3P3_DanielElvir.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale>
 using namespace std;
 void menu() {
-    cout << "1. Aproximacion de PI" << endl;
-    cout << "2. Encontrar Numeros de la secuencia de Pell" << endl;
-    cout << "3. Encontrar Numeros Amigos en un Rango" << endl;
+    setlocale(LC_ALL, "spanish");
+    cout << "MENÚ PRINCIPAL LAB 3 DANIEL ELVIR" << endl;
+    cout << "1. Aproximación de PI" << endl;
+    cout << "2. Encontrar Números de la secuencia de Pell" << endl;
+    cout << "3. Encontrar Números Amigos en un Rango" << endl;
     cout << "4. Salir" << endl;
-    cout << "Escoja una de las opciones validas" << endl;
+    cout << "Escoja una de las opciones válidas" << endl;
 }
 
 void ejercicio1() {    
+    setlocale(LC_ALL, "spanish");
     double puntos;
     double puntosDentro=0;
     double valorPI;
@@ -29,11 +33,11 @@ void ejercicio1() {
 
             valorPI = 4 * (puntosDentro / puntos);
 
-            cout << "Estimacion de PI usando Montecarlo con " << puntos << " puntos generados es: " << valorPI << endl;
+            cout << "Estimación de PI usando Montecarlo con " << puntos << " puntos generados es: " << valorPI << endl;
             cout << "" << endl;
-            break;
+            verify = false;
         } else {
-            cout << "Ingrese un numero valido" << endl;
+            cout << "Ingrese un número válido" << endl;
             cout << "Ingrese el total de puntos que se deben generar (tiene que ser mayor o igual a 1000000): ";
             cin >> puntos;
         }
@@ -58,13 +62,37 @@ void ejercicio2() {
         res = calcularPell(i);
         cout << "P(" << i << ") = " << res << endl;
     }
+    cout << "" << endl;
+}
+
+int sumaDivisoresPropios(int n) {
+    int sumaTotal=1;
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            sumaTotal += i;
+        }
+    }
+    return sumaTotal;
 }
 
 void ejercicio3() {
-    
+    setlocale(LC_ALL, "spanish");
+    int num2;
+    int num3;
+    for (int num1 = 1; num1 <= 10000; num1++) {
+        num2 = sumaDivisoresPropios(num1);
+        num3 = sumaDivisoresPropios(num2);
+        if (num2!=num1 && num1==num3){
+            if (num2 > num1) {
+                cout << num1 << " y " << num2 << " son números amigos." << endl;
+            }
+        }
+    }
+    cout << "" << endl;
 }
 
 int main() {
+    setlocale(LC_ALL, "spanish");
     int opcion;
     bool seguir = true;
     while (seguir){
@@ -86,7 +114,7 @@ int main() {
                 seguir = false;
                 break;
             default:
-                cout << "INGRESE UNA DE LAS OPCIONES VALIDAS" << endl;
+                cout << "INGRESE UNA DE LAS OPCIONES VÁLIDAS" << endl;
                 break;
         }
     }
